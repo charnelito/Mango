79df8b7d47dda4dce7f57658802502dae6137e1e
Charnel Catipay <ccatipay@gmail.com>
2023-08-31 14:08:37 +0800

    20. Common Response

--- Mango.Services.CouponAPI/Migrations/20230831005534_AddCouponToDb.Designer.cs
+++ Mango.Services.CouponAPI/Migrations/20230831005534_AddCouponToDb.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mango.Services.CouponAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20230830074759_AddCouponToDb")]
+    [Migration("20230831005534_AddCouponToDb")]
     partial class AddCouponToDb
     {
         /// <inheritdoc />
@@ -19,7 +19,7 @@ namespace Mango.Services.CouponAPI.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.0-preview.6.23329.4")
+                .HasAnnotation("ProductVersion", "7.0.10")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
